cc09281fd79327ea79323828ec616e0f42c41c65
doremyy <30539627+doremyy@users.noreply.github.com>
2025-05-10 12:48:43 -0400

    reorganizing

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "contact": "@iesua on Discord",
+    "id": "TeamEclecticAnthologists",
+    "patches": {
+        "th20-noAI": "ai asset replacement patch for th20"
+    },
+    "servers": [
+        "https://mirrors.thpatch.net/TeamEclecticAnthologists/"
+    ],
+    "title": "Team Eclectic Anthologists' Patches",
+}
---
+++ th20-noAI/patch.js
@@ -0,0 +1,10 @@
+{
+    "id": "th20-noAI",
+    "servers": [
+        "https://mirrors.thpatch.net/TeamEclecticAnthologists/th20-noAI/"
+    ],
+    "title": "ai asset replacement patch for th20",
+    "dependencies": [
+        "nmlgc/base_tsa"
+    ]
+}
