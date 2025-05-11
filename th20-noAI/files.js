83ff88269f3e5c2e75f3a6ed89898272edf08fd4
doremyy <30539627+doremyy@users.noreply.github.com>
2025-05-11 19:43:37 -0400

    Create files.js

---
+++ th20-noAI/files.js
@@ -0,0 +1,15 @@
+{
+	"patch.js": 1889436905,
+	"th20/background/stage02/st02a.png": 3685495290,
+	"th20/background/stage02/st02b00.png": 933456759,
+	"th20/background/stage02/st02c00.png": 1500677830,
+	"th20/background/stage03/st03a00.png": 3581714660,
+	"th20/background/stage03/st03b00.png": 3768440780,
+	"th20/background/stage03/st03c00.png": 3123529634,
+	"th20/background/stage03/st03d00.PNG": 2558922850,
+	"th20/front/front00.PNG": 1362668156,
+	"th20/loading/sig1280.png": 2835888346,
+	"th20/stgenm/stage01/cardbg0.png": 3635074696,
+	"th20/stgenm/stage03/cdbg03a00.png": 2307152424,
+	"th20/stgenm/stage03/cdbg03b.png": 4179643236
+}
